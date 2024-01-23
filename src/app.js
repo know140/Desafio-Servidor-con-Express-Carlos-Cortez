3ae71e0b86ab2bdede878fffe41fbcd8f7ef7aaf
Carlos Cortez <thehome@live.com.ar>
2024-01-23 15:57:41 -0300

    app local muestra en pantalla

--- src/app.js
+++ src/app.js
@@ -1,10 +1,19 @@
-const express = require (`express`);
+const express = require('express');
+const ProductManager = require('./ProductManager');
 
 const server = express();
-const port = 3005;
+const port = 30010;
 
-server.get(`/`, (req, res)=>{
-    res.send(`Hola mundo`)
-})
+// Asegúrate de tener la ruta correcta del archivo JSON
+const manager = new ProductManager('./src/products.json');
 
-server.listen(port, ()=>console.log(`server up and running on port ${port}`));
+server.get('/', (req, res) => {
+    res.send('Hola mundo');
+});
+
+server.get('/products', async (req, res) => {
+    const products = await manager.getProducts();
+    res.send(products);
+});
+
+server.listen(port, () => console.log(`Server up and running on port ${port}`));
